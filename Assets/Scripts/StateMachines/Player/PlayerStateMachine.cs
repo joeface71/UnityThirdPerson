03157989fae1700573549611b9bef339277df75f
joeface71 <joefacenda@yahoo.com>
2022-08-11 17:01:49 -0400

    imported character and animation

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -1,5 +1,9 @@
+using UnityEngine;
 public class PlayerStateMachine : StateMachine
 {
+    [field: SerializeField]
+    public InputReader InputReader { get; private set; }
+
     private void Start()
     {
         SwitchState(new PlayerTestState(this));
--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -2,28 +2,32 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    float timer = 5f;
+    float timer;
 
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
+        stateMachine.InputReader.JumpEvent += OnJump;
         Debug.Log("Enter");
     }
 
     public override void Exit()
     {
+        stateMachine.InputReader.JumpEvent -= OnJump;
         Debug.Log("Exit");
     }
 
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
+        timer += deltaTime;
         Debug.Log(timer);
 
-        if (timer <= 0)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
+
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
