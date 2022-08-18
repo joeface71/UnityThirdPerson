f1e3571559239fba26a110575b0c28d95c09ba2f
joeface71 <joefacenda@yahoo.com>
2022-08-18 16:51:46 -0400

    Player Attacking State

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class InputReader : MonoBehaviour, Controls.IPlayerActions
 {
+    public bool IsAttacking { get; private set; }
+
     public Vector2 MovementValue { get; private set; }
 
     public event Action JumpEvent;
@@ -60,5 +62,20 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
     {
         if (!context.performed) return;
         CancelEvent?.Invoke();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        // using bool with performed and cancelled allows attack to continue while button is pressed
+        if (context.performed)
+        {
+            IsAttacking = true;
+        }
+        else if (context.canceled)
+        {
+            IsAttacking = false;
+        }
+
+
     }
 }
