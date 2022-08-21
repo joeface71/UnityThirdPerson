457c94f0a2d0ae22988c5a1ae8a7aeb36ae17044
joeface71 <joefacenda@yahoo.com>
2022-08-21 07:52:21 -0400

    First half of implementing impact states

--- Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
@@ -30,4 +30,16 @@ public abstract class PlayerBaseState : State // Since this is abstract we dont 
         stateMachine.transform.rotation = Quaternion.LookRotation(lookPos);
     }
 
+    protected void ReturnToLocomotion()
+    {
+        if (stateMachine.Targeter.CurrentTarget != null)
+        {
+            stateMachine.SwitchState(new PlayerTargetingState(stateMachine));
+        }
+        else
+        {
+            stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+        }
+    }
+
 }
